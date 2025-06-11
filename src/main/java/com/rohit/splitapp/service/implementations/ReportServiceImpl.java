014388b8b063dfd72c0ed6250f2242f40ec3d7a8
Jigar Prajapati <149820949+jdp8951@users.noreply.github.com>
2025-06-11 16:28:25 +0530

    Last Updated ReportServiceImpl.java

--- src/main/java/com/rohit/splitapp/service/implementations/ReportServiceImpl.java
+++ src/main/java/com/rohit/splitapp/service/implementations/ReportServiceImpl.java
@@ -5,9 +5,11 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 
+import com.rohit.splitapp.exception.BadRequestException;
 import com.rohit.splitapp.exception.DataNotFoundException;
 import com.rohit.splitapp.persistence.dto.report.ReportDTO;
 import com.rohit.splitapp.persistence.dto.report.TempReport;
@@ -18,9 +20,8 @@ import com.rohit.splitapp.repository.GroupRepository;
 import com.rohit.splitapp.repository.UserRepository;
 import com.rohit.splitapp.service.interfaces.ReportService;
 
-import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -40,9 +41,14 @@ public class ReportServiceImpl implements ReportService {
     @Autowired
     private ExpenseShareRepository expenseShareRepository;
 
+    @Value("${my.reportFilePath}")
+    private String REPORT_FILE_PATH;
+
     @Override
     public List<ReportDTO> generateReport(UUID groupId) {
+
         Group group = groupRepository.findById(groupId).orElseThrow(() -> new DataNotFoundException("Group not found"));
+        // preparing report
         List<TempReport> tempReportList = groupRepository.generateReportById(group.getId());
         List<ReportDTO> reportDTOS = new ArrayList<>();
 
@@ -52,78 +58,98 @@ public class ReportServiceImpl implements ReportService {
             reportDTO.setExpenseName(tempReport.getExpenseName());
             reportDTO.setGroupName(tempReport.getGroupName());
             reportDTO.setTotalExpenseAmount(tempReport.getTotalExpenseAmount());
+
+//          getting contributors
             List<String> contributors = expenseShareRepository.findPayersById(tempReport.getExpenseId());
             reportDTO.setExpenseContributors(contributors);
             reportDTOS.add(reportDTO);
         }
+
         return reportDTOS;
     }
 
     @Override
-    public byte[] exportReport(UUID groupId, String fileType) {
+    public String exportReport(UUID groupId, String fileType) {
         User user = authorizationService.getAuthorizedUser();
         Group group = groupRepository.findById(groupId).orElseThrow(() -> new DataNotFoundException("Group not found"));
-        if (user == null || user.getId() != group.getUser().getId()) throw new AccessDeniedException("Access Denied");
 
-        if ("XLSX".equalsIgnoreCase(fileType)) {
-            return exportToXLSX(groupId);
-        } else if ("CSV".equalsIgnoreCase(fileType)) {
-            return exportToCSV(groupId);
-        } else {
+        if(user == null || user.getId() != group.getUser().getId()) throw new AccessDeniedException("Access Denied");
+
+        if(fileType.equals("XLSX"))
+            exportToXLSX(groupId);
+        else if (fileType.equals("CSV"))
+            exportToCSV(groupId);
+        else
             throw new RuntimeException("Invalid file type");
-        }
+
+        return "File successfully created at path - " + REPORT_FILE_PATH;
     }
 
-    private byte[] exportToCSV(UUID groupId) {
+    private void exportToCSV(UUID groupId) {
+
         List<TempReport> tempReportList = groupRepository.generateReportById(groupId);
         List<ReportDTO> reportDTOS = new ArrayList<>();
+
         for (TempReport tempReport : tempReportList) {
             ReportDTO reportDTO = new ReportDTO();
             reportDTO.setExpenseOwner(tempReport.getExpenseOwner());
             reportDTO.setExpenseName(tempReport.getExpenseName());
             reportDTO.setGroupName(tempReport.getGroupName());
             reportDTO.setTotalExpenseAmount(tempReport.getTotalExpenseAmount());
+
+//          getting contributors
             List<String> contributors = expenseShareRepository.findPayersById(tempReport.getExpenseId());
             reportDTO.setExpenseContributors(contributors);
             reportDTOS.add(reportDTO);
         }
 
-        StringBuilder csvData = new StringBuilder();
-        csvData.append("groupName,expenseName,expenseOwner,expenseContributors,totalExpenseAmount\n");
-        for (ReportDTO reportDTO : reportDTOS) {
-            csvData.append("\"").append(reportDTO.getGroupName()).append("\",");
-            csvData.append("\"").append(reportDTO.getExpenseName()).append("\",");
-            csvData.append("\"").append(reportDTO.getExpenseOwner()).append("\",");
-            csvData.append("\"").append(String.join(",", reportDTO.getExpenseContributors())).append("\",");
-            csvData.append(reportDTO.getTotalExpenseAmount()).append("\n");
+        try (FileOutputStream fos = new FileOutputStream(REPORT_FILE_PATH + "data.csv")) {
+            StringBuilder csvData = new StringBuilder();
+            csvData.append("groupName,expenseName,expenseOwner,expenseContributors,totalExpenseAmount\n");
+            for (ReportDTO reportDTO : reportDTOS) {
+                csvData.append("\"").append(reportDTO.getGroupName()).append("\",");
+                csvData.append("\"").append(reportDTO.getExpenseName()).append("\",");
+                csvData.append("\"").append(reportDTO.getExpenseOwner()).append("\",");
+                csvData.append("\"").append(String.join(",", reportDTO.getExpenseContributors())).append("\",");
+                csvData.append(reportDTO.getTotalExpenseAmount()).append("\n");
+            }
+            fos.write(csvData.toString().getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        return csvData.toString().getBytes(StandardCharsets.UTF_8);
+
     }
 
-    private byte[] exportToXLSX(UUID groupId) {
+    private void exportToXLSX(UUID groupId) {
         List<TempReport> tempReportList = groupRepository.generateReportById(groupId);
         List<ReportDTO> reportDTOS = new ArrayList<>();
+
         for (TempReport tempReport : tempReportList) {
             ReportDTO reportDTO = new ReportDTO();
             reportDTO.setExpenseOwner(tempReport.getExpenseOwner());
             reportDTO.setExpenseName(tempReport.getExpenseName());
             reportDTO.setGroupName(tempReport.getGroupName());
             reportDTO.setTotalExpenseAmount(tempReport.getTotalExpenseAmount());
+
+//          getting contributors
             List<String> contributors = expenseShareRepository.findPayersById(tempReport.getExpenseId());
             reportDTO.setExpenseContributors(contributors);
             reportDTOS.add(reportDTO);
         }
 
-        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+        try (FileOutputStream outputStream = new FileOutputStream(REPORT_FILE_PATH + "data.xlsx")) {
+
             Workbook workbook = new XSSFWorkbook();
             Sheet sheet = workbook.createSheet("Data");
             int rowNum = 0;
+
             Row headerRow = sheet.createRow(rowNum++);
             headerRow.createCell(0).setCellValue("groupName");
             headerRow.createCell(1).setCellValue("expenseName");
             headerRow.createCell(2).setCellValue("expenseOwner");
             headerRow.createCell(3).setCellValue("expenseContributors");
             headerRow.createCell(4).setCellValue("totalExpenseAmount");
+
             for (ReportDTO reportDTO : reportDTOS) {
                 Row row = sheet.createRow(rowNum++);
                 row.createCell(0).setCellValue(reportDTO.getGroupName());
@@ -134,9 +160,11 @@ public class ReportServiceImpl implements ReportService {
             }
             workbook.write(outputStream);
             workbook.close();
-            return outputStream.toByteArray();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
     }
+
+
 }
