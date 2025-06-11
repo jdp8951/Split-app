235a8b1dbc65db8b6599c33b372184d9e88cb06c
Jigar Prajapati <149820949+jdp8951@users.noreply.github.com>
2025-06-11 16:27:32 +0530

    Latest ReportService.java

--- src/main/java/com/rohit/splitapp/service/interfaces/ReportService.java
+++ src/main/java/com/rohit/splitapp/service/interfaces/ReportService.java
@@ -1,12 +1,14 @@
 package com.rohit.splitapp.service.interfaces;
 
 import java.util.UUID;
+
 import com.rohit.splitapp.persistence.dto.report.ReportDTO;
+
 import java.util.List;
 
 public interface ReportService {
 
     List<ReportDTO> generateReport(UUID groupId);
 
-    byte[] exportReport(UUID groupId, String fileType); // fileType: "XLSX" or "CSV"
+    String exportReport(UUID groupId, String fileType);
 }
